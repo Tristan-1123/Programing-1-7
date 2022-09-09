5c962c65af4e81303f95b4ebb5943089ad6db262
Tristan-1123 <112727293+Tristan-1123@users.noreply.github.com>
2022-09-09 14:21:16 -0500

    Add files via upload

---
+++ prog52a.py
@@ -0,0 +1,11 @@
+length = int( input("Please enter length: ")) 
+width = int (input("Please enter Width: "))
+
+area = length * width
+perim = 2 * length + 2 * width
+
+print("the length is ", length)
+print("the Width is ", width)
+
+print("\nthe area is ", area)
+print("the Perimiter is ", perim)
