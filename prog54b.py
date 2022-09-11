b488814534716caace33dd37876e17b0df708a05
Tristan-1123 <112727293+Tristan-1123@users.noreply.github.com>
2022-09-11 15:59:00 -0500

    Add files via upload

---
+++ prog54b.py
@@ -0,0 +1,10 @@
+A = float (input("Please enter a three digit number:"))
+B = float (input("Please enter a three digit number:"))
+C = float (input("Please enter a three digit number:"))
+D = float (input("Please enter a three digit number:"))
+
+E = A + B + C + D
+F = E / 4
+
+print("\nThe sum of the numbers is ",E)
+print("\nThe Average of the numbers is ", F)
