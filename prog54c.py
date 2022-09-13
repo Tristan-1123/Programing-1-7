06ec5b0fe7e33a8a7e3f99217094fd905caf96e4
Tristan-1123 <112727293+Tristan-1123@users.noreply.github.com>
2022-09-12 23:06:42 -0500

    Add files via upload

---
+++ prog54c.py
@@ -0,0 +1,10 @@
+diameter = int (input("input diameter of circle:"))
+
+pi = 3.14159
+radius = diameter / 2
+circumference =  pi * 2 * radius
+area = pi * radius**2
+
+print ("\nThe radius is ",radius)
+print (" \nThe circumference is ", circumference)
+print (" \nThe area is ", area)
